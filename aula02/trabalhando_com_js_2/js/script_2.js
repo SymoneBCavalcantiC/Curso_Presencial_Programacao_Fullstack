32399420bf3512dcf0b0f98830740d49c1e37a8c
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2024-01-09 17:11:08 -0300

    respostas aos exercícios de casa, parte 2

---
+++ aula02/trabalhando_com_js_2/js/script_2.js
@@ -0,0 +1,5 @@
+var myHeader = document.getElementById("myHeader");
+
+myHeader.innerHTML = "Esse texto vem do JS!";
+
+myHeader.style.color = "blue";
---
+++ aula02/trabalhando_com_js_2/js/script_4.js
@@ -0,0 +1,6 @@
+var idade = 20;
+if (idade >= 18){
+    document.write("Você é MAIOR de idade!");
+} else {
+    document.write("Você é MENOR de idade!")
+};
