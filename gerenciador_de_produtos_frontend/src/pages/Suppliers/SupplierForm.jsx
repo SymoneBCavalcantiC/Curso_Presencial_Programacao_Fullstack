8e845ea9f13f922c41a43b89fefbf26a9e64e51b
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2024-03-07 06:36:22 -0300

    Atualização aula 7, parte 1

--- gerenciador_de_produtos_frontend/src/pages/Suppliers/SupplierForm.jsx
+++ gerenciador_de_produtos_frontend/src/pages/Suppliers/SupplierForm.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from '../../api'
 
 
@@ -8,6 +8,17 @@ const SupplierForm = () => {
 
     const [supplier, setSupplier] = useState({name: '', cnpj: '', email: ''})
     const navigate = useNavigate()
+    const {id} = useParams()
+
+    useEffect(() => {
+        if (id) {
+            axios.get(`/suppliers/${id}`)
+            .then(response => {
+                setSupplier(response.data)
+            })
+            .catch(error => console.error('Erro ao buscar fornecedor', error))
+        }
+    }, [id])
 
     function handleChange(event) {
         const { name, value } = event.target
@@ -16,18 +27,20 @@ const SupplierForm = () => {
 
     function handleSubmit(event) {
         event.preventDefault()
+        const method = id ? 'put' : 'post'
+        const url = id ? `/suppliers/${id}` : '/suppliers'
 
-        axios.post('/suppliers', supplier)
+        axios[method](url, supplier)
             .then(() => {
-                alert("Fornecedor adicionado com sucesso!")
-                navigate("/")
+                alert(`Fornecedor ${id ? 'atualizado' : 'adicionado'} com sucesso!`)
+                navigate("/listar-fornecedores")
             })
             .catch(error => console.error("Houve um erro: ", error));
     }
 
   return (
     <div className='container mt-5'>
-        <h2>Adicionar Fornecedor</h2>
+        <h2>{id ? 'Editar' : 'Adicionar'} Fornecedor</h2>
         <form onSubmit={handleSubmit}>
             <div className='form-group'>
                 <label htmlFor="name">Nome do Fornecedor:</label>
@@ -44,7 +57,7 @@ const SupplierForm = () => {
                 <input type='text' className='form-control' id='email' name='email' value={supplier.email} onChange={handleChange} required />
             </div>
 
-            <button type="submit" className="btn btn-success">Adicionar</button>
+            <button type="submit" className="btn btn-success">{id ? 'Atualizar' : 'Adicionar'}</button>
         </form>
         
     </div>
